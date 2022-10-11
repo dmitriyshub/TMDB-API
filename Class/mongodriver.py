6de53febe925a10efe40db566d91c179256e85b6
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-12 01:16:28 +0300

    testing pymongo

--- Class/mongodriver.py
+++ Class/mongodriver.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import json
 import gridfs
 
 
@@ -8,7 +9,7 @@ class MongoDriver:
 
     def __init__(self):
         # Connect to the server with the hostName and portNumber.
-        self.mongo = MongoClient("localhost", 27017)
+        self.mongo = MongoClient("localhost", 27017, maxPoolSize=50)
 
         # Connect to the Database where the images will be stored.
         self.database = self.mongo['movie_posters']
@@ -21,7 +22,7 @@ class MongoDriver:
 
     def add_image(self):
         # define an image object with the location. TODO add movie name from imdb from the api class
-        image_name = "content\poster_fight club.jpeg"
+        image_name = "content\poster_terminal.jpeg"
         # Open the image in read-only format.
         with open(image_name, 'rb') as f:
             contents = f.read()
@@ -30,9 +31,28 @@ class MongoDriver:
     def read_image(self):
         self.database.fi
 
+    def get_tables(self):
+        client = self.mongo
+        d = dict((db, [collection for collection in client[db].list_collection_names()])
+                 for db in client.list_database_names())
+        return json.dumps(d)
+
+    def get_image(self):
+        client = self.mongo
+        db = client.localhost
+        collection = db['movie_posters']
+        cursor = collection
+
+        a = []
+        print(cursor)
+
 
 
 
 if __name__ == "__main__":
     test = MongoDriver()
-    test.add_image()
+    #test.add_image()
+    test2 = test.get_tables()
+    print(test2)
+    test3 = test.get_image()
+    print(test3)

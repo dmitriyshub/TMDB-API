3060b10d57c8bc5a1acb4d19a3defdd345a6a2e1
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-13 15:17:41 +0300

    testing flask

--- Class/app.py
+++ Class/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, json , Response
+from flask import Flask, request, json , Response, render_template, make_response
 from mongoDBDAL import MongoDBDAL
-app = Flask(__name__)
+from TMDBDownloader import TMDBDownloader
+from config import content_temp_path
+from base64 import b64encode, b64decode
 
+app = Flask(__name__)
+mdb = MongoDBDAL("movies", ip='localhost', port=27017)
+TMDB = TMDBDownloader()
 
 @app.route('/')
 def base():
@@ -9,23 +14,29 @@ def base():
                     status=200,
                     mimetype='application/json')
 
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hello, {name}!</h1>'
 
-@app.route('/mongodb', methods=['GET'])
-def mongo_read():
-    data = request.json
-    filename = data['movie_name']
-    if data is None or data == {}:
-        return Response(response=json.dumps({"Error": "Please provide connection information"}),
-                        status=400,
-                        mimetype='application/json')
-    obj1 = MongoDBDAL("movies", ip='localhost', port=27017)
-    response = obj1.read_image_file(filename)
-    json_str = json.dumps({'message': response.decode()})
 
-    return Response(response=json.dumps(response),
-                    status=200,
-                    mimetype='application/json')
+@app.route('/search', methods=['GET', 'POST'])
+def load_insert_item_html():
+    if request.method == 'POST':
+        movie_name = request.form['name']
+        imdb_id , file_name = TMDB.search_downloader(movie_name)
+        mdb.write_image_file(content_temp_path + file_name, movie_name,imdb_id)
+        binary_file = mdb.read_image_file(movie_name)
+        image = b64encode(binary_file).decode("utf-8")
+        src = "data:image/gif;base64," + image
+        return f'<img src={src}>'
+    return render_template('new_form.html')
 
+@app.route('/mongo/<search_string>', methods=['GET'])
+def read(search_string):
+    binary_file = mdb.read_image_file(search_string)
+    image = b64encode(binary_file).decode("utf-8")
+    src = "data:image/gif;base64," + image
+    return f'<img src={src}>'
 
 
 if __name__ == '__main__':

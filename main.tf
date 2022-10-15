9cb63eb1f78f78d3569a4ba2a5ef108e8b9e08bc
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-15 23:05:51 +0300

    terraform files

--- main.tf
+++ main.tf
@@ -1,29 +1,79 @@
 # 1. Create a `main.tf` file in the root of this repository with the `remote` backend and one or more resources defined.
 #   Example `main.tf`:
      # The configuration for the `remote` backend.
-     terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "dmitriyshub"
+#     terraform {
+#       backend "remote" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "dmitriyshub"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "github-actions-tmdb-api"
+#         }
+#       }
+#
+#     }
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "github-actions-tmdb-api"
-         }
-       }
+#     # An example resource that does nothing.
+#     resource "null_resource" "example" {
+#       triggers = {
+#         value = "A example resource that does nothing!"
+#       }
+#     }
+#
+#resource "aws_vpc" "vpc" { # terraform id&name
+#  cidr_block = "172.16.0.0/16" # specify the network
+#  tags = {
+#      Name = "dmitriyshub-vpc" # aws Tag
+#  }
+#}
 
-     }
+terraform {
 
-     # An example resource that does nothing.
-     resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+  cloud {
+    organization = "dmitriyshub"
 
-resource "aws_vpc" "vpc" { # terraform id&name
-  cidr_block = "172.16.0.0/16" # specify the network
+    workspaces {
+      name = "github-actions-tmdb-api"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
+
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
   tags = {
-      Name = "dmitriyshub-vpc" # aws Tag
+    Name = var.instance_name
   }
 }

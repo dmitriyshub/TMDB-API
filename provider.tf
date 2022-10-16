fb6f699debc2f50fcc1cf82d6c9fe0c4f594cf72
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-17 01:31:41 +0300

    provider.tf file

---
+++ provider.tf
@@ -0,0 +1,40 @@
+# 1. Create a `main.tf` file in the root of this repository with the `remote` backend and one or more resources defined.
+#   Example `main.tf`:
+     # The configuration for the `remote` backend.
+#     terraform {
+#       backend "remote" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "dmitriyshub"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "github-actions-tmdb-api"
+#         }
+#       }
+#
+#     }
+#######terraform###########init###########terraform#############
+terraform {
+
+  cloud {
+    organization = "dmitriyshub"
+
+    workspaces {
+      name = "github-actions-tmdb-api"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
+
+
+provider "aws" {
+  region = var.region
+}

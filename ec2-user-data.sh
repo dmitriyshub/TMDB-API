6f209c6c55da326e582dbc4c29d956965fda3746
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-16 16:28:39 +0300

    add ssm to userdata

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-yum update -y
-amazon-linux-extras install docker
-service docker start
-usermod -a -G docker ec2-user
-sudo chkconfig docker on
-sudo yum install -y git
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+account='[default]'
+output='output = json'
+region='region = eu-central-1'
+printf  "$account" "$output" "$region" > /home/$USER/.aws/config
+aws ssm get-parameter --name tmdb-api --with-decryption --query 'Parameter.Value' > .env
 
-mkdir /home/ec2-user/tmdb-api && cd /home/ec2-user/tmdb-api || cd /
+yum update -y
+yum install -y git
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# mkdir /home/ec2-user/tmdb-api && cd /home/ec2-user/tmdb-api || cd /
 git clone https://github.com/dmitriyshub/TMDB-API.git
+cd TMDB-API
 docker-compose up -d
 
 

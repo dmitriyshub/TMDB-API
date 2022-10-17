e919563f33808cf519affc6c823a7f60a0690239
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-17 14:37:47 +0300

    add erase history and result file

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+#Start
 yum update -y
 yum install -y git
 git clone https://github.com/dmitriyshub/TMDB-API.git /home/ec2-user/app
 mkdir /home/ec2-user/.aws
 mkdir /home/ec2-user/app/secret
+
 #configure aws region
 export AWS_DEFAULT_REGION=eu-central-1
 echo '[default]' >> /home/ec2-user/.aws/config
 echo 'output = json' >> /home/ec2-user/.aws/config
 echo 'region = eu-central-1' >> /home/ec2-user/.aws/config
-sleep 10
+sleep 5
+
 #get api token .env
 aws ssm get-parameter --name /dev/api_key --with-decryption --query 'Parameter.Value' | cut -d "\"" -f 2 > /home/ec2-user/app/secret/.env
 sudo chown -R ec2-user:ec2-user /home/ec2-user/app /home/ec2-user/.aws /home/ec2-user/app/secret /home/ec2-user/.aws/config /home/ec2-user/app/secret/.env
@@ -23,7 +26,13 @@ curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compos
 chmod +x /usr/local/bin/docker-compose
 chown root:docker /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# mkdir /home/ec2-user/tmdb-api && cd /home/ec2-user/tmdb-api || cd /
 docker-compose -f /home/ec2-user/app/docker-compose.yml up -d
+
+# These lines erase any history or security information that may have accidentally been left on the instance when the image was taken.
+echo 'UserData has been successfully executed. ' >> /home/ec2-user/result
+find -wholename "/root/.*history" -wholename "/home/*/.*history" -exec rm -f {} \;
+find / -name 'authorized_keys' -exec rm -f {} \;
+rm -rf /var/lib/cloud/data/scripts/*
+
+
 

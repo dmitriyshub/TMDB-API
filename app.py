a8cf9a4d2bacd783127de68f854b7ed373a22058
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-18 20:00:38 +0300

    adding create by url and elif statement for No such file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, json , Response, render_template
+from flask import Flask, request, json , Response, render_template #, send_file
 from mongoDBDAL import MongoDBDAL
 from TMDBDownloader import TMDBDownloader
 #from secret.config import content_temp_path
@@ -18,8 +18,7 @@ def base():
 def user(name):
     return f'<h1>Hello, {name}!</h1>'
 
-
-@app.route('/search', methods=['GET', 'POST'])
+@app.route('/create', methods=['GET', 'POST'])
 def search_and_download():
     if request.method == 'POST':
         movie_name = request.form['name']
@@ -29,11 +28,6 @@ def search_and_download():
                 imdb_id, file_name = TMDB.search_downloader(movie_name)
             except:
                 return render_template('new_form.html')
-                # return Response(response=json.dumps({"Status": "API Error Try Again"}),
-                #                 status=403,
-                #                 mimetype='application/json'
-                #                 )
-
             mdb.write_image_file(content_temp_path + file_name, movie_name, imdb_id)
             binary_file = mdb.read_image_file(movie_name)
             image = b64encode(binary_file).decode("utf-8")
@@ -46,7 +40,31 @@ def search_and_download():
 
     return render_template('new_form.html')
 
-@app.route('/mongo/search', methods=['GET','POST'])
+@app.route('/create/url/<search_string>', methods=['GET', 'POST'])
+def search_and_download_url(search_string):
+    if request.method == 'POST' or request.method == 'GET':
+
+        binary_file = mdb.read_image_file(search_string)
+        if binary_file == None:
+            try:
+                imdb_id, file_name = TMDB.search_downloader(search_string)
+            except:
+                return Response(response=json.dumps({"Status": "API Error Try Again"}),
+                                status=403,
+                                mimetype='application/json')
+            mdb.write_image_file(content_temp_path + file_name, search_string, imdb_id)
+            binary_file = mdb.read_image_file(search_string)
+            image = b64encode(binary_file).decode("utf-8")
+            src = "data:image/gif;base64," + image
+            return f'<img src={src}>'
+        else:
+            image = b64encode(binary_file).decode("utf-8")
+            src = "data:image/gif;base64," + image
+            return f'<img src={src}>'
+
+
+
+@app.route('/read', methods=['GET','POST'])
 def read_form():
     if request.method == 'POST':
         search_string = request.form['name']
@@ -57,7 +75,7 @@ def read_form():
             return f'<img src={src}>'
     return render_template('read_form.html')
 
-@app.route('/mongo/search/url/<search_string>', methods=['GET'])
+@app.route('/read/url/<search_string>', methods=['GET'])
 def read_url(search_string):
     binary_file = mdb.read_image_file(search_string)
     if binary_file != None:
@@ -68,7 +86,7 @@ def read_url(search_string):
                     status=404,
                     mimetype='application/json')
 
-@app.route('/mongo/delete',methods=['GET','POST'])
+@app.route('/delete',methods=['GET','POST'])
 def delete_form():
     if request.method == 'POST':
         movie_name = request.form['name']
@@ -81,6 +99,10 @@ def delete_form():
             return Response(response=json.dumps(delete_movie),
                             status=404,
                             mimetype='application/json')
+        elif delete_movie['Status'] == 'No such file':
+            return Response(response=json.dumps({"Status": "No such file"}),
+                            status=404,
+                            mimetype='application/json')
         else:
             return Response(response=json.dumps({"Status": "Something went Wrong"}),
                             status=404,
@@ -88,7 +110,7 @@ def delete_form():
 
     return render_template('delete_form.html')
 
-@app.route('/mongo/delete/url/<delete_string>', methods=['DELETE'])
+@app.route('/delete/url/<delete_string>', methods=['GET'])
 def delete_url(delete_string):
     delete_movie = mdb.del_image_file(delete_string)
     if delete_movie['Status'] == 'Successfully Deleted':
@@ -99,12 +121,16 @@ def delete_url(delete_string):
         return Response(response=json.dumps(delete_movie),
                         status=404,
                         mimetype='application/json')
+    elif delete_movie['Status'] == 'No such file':
+        return Response(response=json.dumps({"Status": "No such file"}),
+                        status=404,
+                        mimetype='application/json')
     else:
         return Response(response=json.dumps({"Status": "Something went Wrong"}),
                         status=404,
                         mimetype='application/json')
 
-@app.route('/mongo/update', methods=['GET', 'POST', 'PUT'])
+@app.route('/update', methods=['GET', 'POST', 'PUT'])
 def update_form():
     if request.method == 'POST' or request.method == 'PUT':
         movie_name = request.form['name']
@@ -115,10 +141,17 @@ def update_form():
             return Response(response=json.dumps(update_movie),
                             status=200,
                             mimetype='application/json')
+
         elif update_movie['Status'] == 'Nothing was Updated':
             return Response(response=json.dumps(update_movie),
                             status=404,
                             mimetype='application/json')
+
+        elif update_movie['Status'] == 'No such file':
+            return Response(response=json.dumps({"Status": "No such file"}),
+                            status=404,
+                            mimetype='application/json')
+
         else:
             return Response(response=json.dumps({"Status": "Something went Wrong"}),
                             status=404,
@@ -131,3 +164,48 @@ def update_form():
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # @app.route('/create/api', methods=['GET', 'POST'])
+    # def search_and_download_api():
+    #     data = request.json
+    #     movie_name = data['name']
+    #     binary_file = mdb.read_image_file(movie_name)
+    #     if binary_file == None:
+    #         try:
+    #             imdb_id, file_name = TMDB.search_downloader(movie_name)
+    #         except:
+    #             return Response(response=json.dumps({"Status": "API Error Try Again"}),
+    #                             status=403,
+    #                             mimetype='application/json')
+    #         mdb.write_image_file(content_temp_path + file_name, movie_name, imdb_id)
+    #         binary_file = mdb.read_image_file(movie_name)
+    #         image = b64encode(binary_file).decode("utf-8")
+    #
+    #         return send_file(content_temp_path+ file_name, mimetype='image/gif')
+    #     else:
+    #         try:
+    #             imdb_id, file_name = TMDB.search_downloader(movie_name)
+    #         except:
+    #             return Response(response=json.dumps({"Status": "API Error Try Again"}),
+    #                             status=403,
+    #                             mimetype='application/json')
+    #         binary_file = mdb.read_image_file(movie_name)
+    #         image = b64encode(binary_file).decode("utf-8")
+    #         return send_file(content_temp_path+ file_name, mimetype='image/gif')
